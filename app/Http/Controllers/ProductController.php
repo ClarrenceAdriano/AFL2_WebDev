438bcb17379dc86a3a7ffef0a8d367bc712a4bbc
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2025-11-04 08:32:44 +0700

    pagination, add datas

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,7 +22,7 @@ class ProductController extends Controller
                     });
             })
             ->latest()
-            ->paginate(4);
+            ->paginate(12);
 
         return view('products', [
             'products' => $products,
